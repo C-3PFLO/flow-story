9214f5222a75ea7bca6832338a4bc058c43c1f65
C-3PFLO <c.3pflo.1@gmail.com>
2023-03-01 23:01:46 -0500

    more scaffold

---
+++ src/view/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Page } from './components/Page';
+
+import './App.css';
+
+/**
+ * Get component function
+ * @public
+ * @return {Function}
+ */
+export function App() {
+    return (
+        <>
+            <h1>flow-story</h1>
+            <Page/>
+        </>
+    );
+}
---
+++ src/view/components/Page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Page.css';
+
+/**
+ * Get component function
+ * @public
+ * @return {Function}
+ */
+export function Page() {
+    return (
+        <div className='page'>
+            Hello World!
+        </div>
+    );
+}
