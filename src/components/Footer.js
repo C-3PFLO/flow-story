be285268ab5208bc6b04e8aed15dbe2e214d147e
C-3PFLO <c.3pflo.1@gmail.com>
2023-03-04 23:34:20 -0500

    tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,19 @@
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 export default function Footer() {
     return (
-        <>
         <Typography variant="caption">
-            {/* <span>&copy; 2023 C-3PFLO TBD</span> */}
-            <div>Built by C-3PFLO - beep beep, boop boop</div>
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                    {/* <span>&copy; 2023 C-3PFLO TBD</span> */}
+                    <div>Built by C-3PFLO - Written by Us</div>
+                    <div>beep beep, boop boop</div>
+            </Box>
         </Typography>
-        </>
     )
 }
